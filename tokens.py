fab34a7641f190849f0cd841927837315e6f889d
Yasmeen_sale7 <yasmeensaleh747@gmail.com>
2026-04-25 18:44:44 +0300

    Refactor token definitions and add new tokens

--- tokens.py
+++ tokens.py
@@ -1,46 +1,40 @@
-# tokens.py
-
 NUMBER = 'NUMBER'
 PLUS = 'PLUS'
 MINUS = 'MINUS'
 MUL = 'MUL'
 DIV = 'DIV'
 GE = 'GE'
-LE = 'LE'            # جديد: <=
+LE = 'LE'
+LT = 'LT'
+NE = 'NE'
 INCREMENT = 'INCR'
 DECREMENT = 'DECR'
-LT = 'LT'            # جديد: <
-NE = 'NE'            # جديد: != أو <>
 LPAREN = 'LPAREN'
 RPAREN = 'RPAREN'
-LBRACE = 'LBRACE'    # جديد: {
-RBRACE = 'RBRACE'    # جديد: }
+LBRACE = 'LBRACE'
+RBRACE = 'RBRACE'
 ID = 'ID'
 ASSIGN = 'ASSIGN'
 END = 'END'
 COMMA = 'COMMA'
 IF = 'IF'
-ELSE = 'ELSE'        # جديد
-ELIF = 'ELIF'        # جديد
-FOR = 'FOR'          # جديد
-IN = 'IN'            # جديد: for i in range
-TO = 'TO'            # جديد: for i = 1 to 10
-DO = 'DO'            # جديد: بديل لـ {
+ELSE = 'ELSE'
+ELIF = 'ELIF'
+FOR = 'FOR'
+DO = 'DO'
 PRINT = 'PRINT'
-INPUT = 'INPUT'      # جديد
+INPUT = 'INPUT'
 EOF = 'EOF'
 
 KEYWORDS = {
     'end': END,
     'if': IF,
-    'else': ELSE,     # جديد
-    'elif': ELIF,     # جديد
-    'for': FOR,       # جديد
-    'in': IN,         # جديد
-    'to': TO,         # جديد
-    'do': DO,         # جديد
+    'else': ELSE,
+    'elif': ELIF,
+    'for': FOR,
+    'do': DO,
     'print': PRINT,
-    'input': INPUT    # جديد
+    'input': INPUT
 }
 
 class Token:
